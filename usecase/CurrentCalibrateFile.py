07e1e67479b63868d9063013ab3695bfb0e3edcc
Always <tt20200220i@163.com>
2021-05-19 17:10:59 +0800

    修改编辑系数的方法、一个属性名

--- usecase/CurrentCalibrateFile.py
+++ usecase/CurrentCalibrateFile.py
@@ -7,8 +7,8 @@ from usecase.MergeCalibrateFile import MergeCalibrateFile
 
 class CalibrateFileEdit:
     def __init__(self):
-        self._calibrate_parameter_edit = EditCalibrateParameter()
-        self._depend_edit = EditCalibrateParameterDepends()
+        self._calibrate_parameter_editor = EditCalibrateParameter()
+        self._depend_editor = EditCalibrateParameterDepends()
         self._merge = MergeCalibrateFile()
 
         self._current_file_path = 'file_path'
@@ -127,95 +127,97 @@ class CalibrateFileEdit:
 
     # 文件合并
     def merge_calibrate_file_by_method_two(self, merge_channel_index, another_channel_index, another_parameter_id):
-        merge_channel = self._channels[merge_channel_index]
+        merge_channel = self._current_channels[merge_channel_index]
         self._merge.another_file = self._another_file_path
         new_channel = self._merge.merge_by_method_two(merge_channel, another_channel_index, another_parameter_id)
-        self._channels[merge_channel_index] = new_channel
+        self._current_channels[merge_channel_index] = new_channel
 
     def merge_calibrate_file_by_method_three(self, merge_channel_index, another_channel_index):
-        merge_channel = self._channels[merge_channel_index]
+        merge_channel = self._current_channels[merge_channel_index]
         self._merge.another_file = self._another_file_path
         new_channel = self._merge.merge_by_method_three(merge_channel, another_channel_index)
-        self._channels[merge_channel_index] = new_channel
+        self._current_channels[merge_channel_index] = new_channel
 
-    # 参数信息编辑
-    def modify_parameter_factors(self, parameter_node, segment, value):
-        self._calibrate_parameter_edit.parameter_node = parameter_node
-        self._calibrate_parameter_edit.modify_parameter_factors(value, segment)
-        return self._calibrate_parameter_edit.parameter_node
+    # 参数信息编辑  # TODO 得到是一个复制的结点？要是这样，修改完之后还要找到树中对应的结点然后替代？
+    def modify_parameter_factors(self, channel_index, calibrate_parameter_id, path, segment, new_factors):
+        dependency_leaf_node = self.get_parameter_parent_node(channel_index, calibrate_parameter_id, path)
+        parameter_node = dependency_leaf_node.children[0]
+        self._calibrate_parameter_editor.parameter_node = parameter_node
+        self._calibrate_parameter_editor.modify_parameter_factors(new_factors, segment)
+        return self._calibrate_parameter_editor.parameter_node
 
     def modify_calibrate_parameter_interval(self, parameter_node, segment, value):
-        self._calibrate_parameter_edit.parameter_node = parameter_node
-        self._calibrate_parameter_edit.modify_parameter_interval(value, segment)
-        return self._calibrate_parameter_edit.parameter_node
+        self._calibrate_parameter_editor.parameter_node = parameter_node
+        self._calibrate_parameter_editor.modify_parameter_interval(value, segment)
+        return self._calibrate_parameter_editor.parameter_node
 
     def add_parameter_segment(self, parameter_node, segment):
-        self._calibrate_parameter_edit.parameter_node = parameter_node
-        self._calibrate_parameter_edit.add_parameter_segment(segment)
-        return self._calibrate_parameter_edit.parameter_node
+        self._calibrate_parameter_editor.parameter_node = parameter_node
+        self._calibrate_parameter_editor.add_parameter_segment(segment)
+        return self._calibrate_parameter_editor.parameter_node
 
     def delete_parameter_segment(self, parameter_node, segment):
-        self._calibrate_parameter_edit.parameter_node = parameter_node
-        self._calibrate_parameter_edit.delete_parameter_segment(segment)
-        return self._calibrate_parameter_edit.parameter_node
+        self._calibrate_parameter_editor.parameter_node = parameter_node
+        self._calibrate_parameter_editor.delete_parameter_segment(segment)
+        return self._calibrate_parameter_editor.parameter_node
 
     def add_parameter_node(self, root_node, parent_node):
         leaf_nodes = root_node.leaves
         parameter_id = leaf_nodes[0].parameter_id
-        root_node = self._calibrate_parameter_edit.add_parameter_node(parameter_id, root_node, parent_node)
+        root_node = self._calibrate_parameter_editor.add_parameter_node(parameter_id, root_node, parent_node)
         return root_node
 
     def delete_parameter_node(self, root_node, parameter_node):
-        root_node = self._calibrate_parameter_edit.delete_parameter_node(root_node, parameter_node)
+        root_node = self._calibrate_parameter_editor.delete_parameter_node(root_node, parameter_node)
         return root_node
 
     # 参数分段转化成dict形式便于查找
     def get_segments_dict(self, parameter_node):
-        self._calibrate_parameter_edit.parameter_node = parameter_node
-        segments_dict = self._calibrate_parameter_edit.get_segments_dict()
+        self._calibrate_parameter_editor.parameter_node = parameter_node
+        segments_dict = self._calibrate_parameter_editor.get_segments_dict()
         return segments_dict
 
     # 显示系数曲线
     def show_current_factors_curve(self, segment):
-        self._calibrate_parameter_edit.show_current_factors_curve(segment)
+        self._calibrate_parameter_editor.show_current_factors_curve(segment)
 
-    def show_two_factors_curve(self, modified_segment):
-        self._calibrate_parameter_edit.show_two_factors_curve(modified_segment)
+    def show_two_factors_curve(self, modified_segment, current_segment):
+        self._calibrate_parameter_editor.show_two_factors_curve(modified_segment, current_segment)
 
     # 依赖编辑
     def add_depend(self, root_node, depend_id, parent_id):
-        self._depend_edit.root_node = root_node
-        self._depend_edit.add_depend(parent_id, depend_id)
-        return self._depend_edit.root_node
+        self._depend_editor.root_node = root_node
+        self._depend_editor.add_depend(parent_id, depend_id)
+        return self._depend_editor.root_node
 
     def delete_depend(self, root_node, depend_id):
-        self._depend_edit.root_node = root_node
-        self._depend_edit.delete_depend(depend_id)
-        return self._depend_edit.root_node
+        self._depend_editor.root_node = root_node
+        self._depend_editor.delete_depend(depend_id)
+        return self._depend_editor.root_node
 
     def add_depend_segment_until_leaf(self, root_node, start_depend_node):
-        self._depend_edit.root_node = root_node
-        self._depend_edit.add_depends_segment_nodes_until_leaf(start_depend_node)
-        return self._depend_edit.root_node
+        self._depend_editor.root_node = root_node
+        self._depend_editor.add_depends_segment_nodes_until_leaf(start_depend_node)
+        return self._depend_editor.root_node
 
     def delete_depend_segment(self, root_node, depend_node):
-        self._depend_edit.root_node = root_node
-        self._depend_edit.delete_depend_segment(depend_node)
-        return self._depend_edit.root_node
+        self._depend_editor.root_node = root_node
+        self._depend_editor.delete_depend_segment(depend_node)
+        return self._depend_editor.root_node
 
     def modify_depend_segment(self, lower_num, upper_num, depend_node, root_node):
-        self._depend_edit.root_node = root_node
-        self._depend_edit.modify_depend_segment(lower_num, upper_num, depend_node)
-        return self._depend_edit.root_node
+        self._depend_editor.root_node = root_node
+        self._depend_editor.modify_depend_segment(lower_num, upper_num, depend_node)
+        return self._depend_editor.root_node
 
     # 保存
     def save(self):
         file_handler = FileHandler()
-        file_handler.file_path = self._file_path
-        calibrate_file = file_handler.calibrate_msg_to_file_form(self._channels)
+        file_handler.file_path = self._current_file_path
+        calibrate_file = file_handler.calibrate_msg_to_file_form(self._current_channels)
         file_handler.save(calibrate_file)
 
     def save_as(self, file_path):
         file_handler = FileHandler()
-        calibrate_file = file_handler.calibrate_msg_to_file_form(self._channels)
+        calibrate_file = file_handler.calibrate_msg_to_file_form(self._current_channels)
         file_handler.save_as(calibrate_file, file_path)
