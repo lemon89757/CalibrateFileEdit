dc6ed42f296d90e89d748de22cd31f888edaf683
Always <tt20200220i@163.com>
2021-05-19 17:12:28 +0800

    增加系数编辑界面的presenter

---
+++ presenter/FactorsEditUIPresenter.py
@@ -0,0 +1,50 @@
+class FactorsEditUIPresenter:   # TODO 可将MainUIPresenter传进来？
+    def __init__(self):
+        self._editor = None
+        self._view = None
+
+    @property
+    def view(self):
+        return self._view
+
+    @view.setter
+    def view(self, value):
+        self._view = value
+
+    @property
+    def editor(self):
+        return self._editor
+
+    @editor.setter
+    def editor(self, value):
+        self._editor = value
+
+    def load_current_entry(self):
+        factors = []
+        for entry in self._view.entries:
+            factor = float(entry.get_text())
+            factors.append(factor)
+        return factors
+
+    def show_two_curves(self):
+        current_interval = self._editor.load_choosed_parameter_interval()
+        current_factors = self._editor.load_current_factors()
+        modified_factors = self.load_current_entry()
+        modified_segment = [current_interval, modified_factors]
+        current_segment = [current_interval, current_factors]
+        self._editor.show_two_factors_curve(modified_segment, current_segment)
+
+    def modify_factors(self):
+        channel_index = self._editor.load_channel_index()
+        calibrate_parameter_id = self._editor.load_choosed_calibrate_parameter()
+        path = self._editor.load_parameter_node_path()
+        current_factors = self._editor.load_current_factors()
+        current_interval = self._editor.load_choosed_parameter_interval()
+        segment = [current_interval, current_factors]
+        modified_factors = self.load_current_entry()
+        self._editor.modify_parameter_factors(channel_index, calibrate_parameter_id, path, segment, modified_factors)
+        print("yes")
+
+    def update_modified_factors(self):
+        modified_factors = self.load_current_entry()
+        self._editor.update_modified_factors(modified_factors)
